0a8b1e0e05dc5e08b95b719f3ef41dcc2d505735
ADAME-TECHS <adame.naziff.sql@gmail.com>
2017-10-20 18:43:26 +0200

    je créer un bug

--- src/testJenkins/Main.java
+++ src/testJenkins/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("coucou jenkins me voici !! :)");
+		Systeme.out.println("coucou jenkins me voici !! :)");
 
 	}
 
